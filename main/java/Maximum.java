eaea8149a9d73b80e6084da0bb724985d2334c9e
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-05-12 20:51:45 +0530

    Refactor all three to one Generic Method

--- main/java/Maximum.java
+++ main/java/Maximum.java
@@ -1,28 +1,38 @@
 public class Maximum {
-    public static int maxInteger(Integer firstNumber,Integer secondNumber,Integer thirdNumber) {
-        int max = firstNumber;
-        if(secondNumber.compareTo(max) > 0)
-            max = secondNumber;
-        if(thirdNumber.compareTo(max) > 0)
-            max = thirdNumber;
+
+    public static <T extends Comparable<T>> T printMaximum(T first, T second, T third) {
+        T max = first;
+        if (second.compareTo(max) > 0)
+            max = second;
+        if (third.compareTo(max) > 0)
+            max = third;
         return max;
     }
-
-    public static float maxFloat(Float firstNumber, Float secondNumber, Float thirdNumber) {
-        float max1 = firstNumber;
-        if(secondNumber.compareTo(max1) > 0)
-            max1 = secondNumber;
-        if(thirdNumber.compareTo(max1) > 0)
-            max1 = thirdNumber;
-        return max1;
-    }
-
-    public static String maxString(String firstString, String secondString, String thirdString) {
-        String max2 = firstString;
-        if(secondString.compareTo(max2) > 0)
-            max2 = secondString;
-        if(thirdString.compareTo(max2) > 0)
-            max2 = thirdString;
-        return max2;
-    }
 }
+//    public static int maxInteger(Integer firstNumber,Integer secondNumber,Integer thirdNumber) {
+//        int max = firstNumber;
+//        if(secondNumber.compareTo(max) > 0)
+//            max = secondNumber;
+//        if(thirdNumber.compareTo(max) > 0)
+//            max = thirdNumber;
+//        return max;
+//    }
+//
+//    public static float maxFloat(Float firstNumber, Float secondNumber, Float thirdNumber) {
+//        float max1 = firstNumber;
+//        if(secondNumber.compareTo(max1) > 0)
+//            max1 = secondNumber;
+//        if(thirdNumber.compareTo(max1) > 0)
+//            max1 = thirdNumber;
+//        return max1;
+//    }
+//
+//    public static String maxString(String firstString, String secondString, String thirdString) {
+//        String max2 = firstString;
+//        if(secondString.compareTo(max2) > 0)
+//            max2 = secondString;
+//        if(thirdString.compareTo(max2) > 0)
+//            max2 = thirdString;
+//        return max2;
+//    }
+//}
--- test/java/MaximumTest.java
+++ test/java/MaximumTest.java
@@ -5,63 +5,63 @@ public class MaximumTest {
 
     @Test
     public void maxNumberAtFirstPosition_ReturnTheSameNumber() {
-        int max = Maximum.maxInteger(7,3,4);
+        int max = Maximum.printMaximum(7,3,4);
         Assert.assertEquals(7,max);
         System.out.println("Maximum No at First position:" +max);
     }
 
     @Test
     public void maxNumberAtSecondPosition_ReturnTheSameNumber() {
-        int max = Maximum.maxInteger(4,6,3);
+        int max = Maximum.printMaximum(4,6,3);
         Assert.assertEquals(6,max);
         System.out.println("Maximum No at Second position:" +max);
     }
 
     @Test
     public void maxNumberAtThirdPosition_ReturnTheSameNumber() {
-        int max = Maximum.maxInteger(4,6,9);
+        int max = Maximum.printMaximum(4,6,9);
         Assert.assertEquals(9,max);
         System.out.println("Maximum No at Third position:" +max);
     }
 
     @Test
     public void maxFloatNumberAtFirstPosition_ReturnTheSameNumber() {
-        float max1 = Maximum.maxFloat(8.0f,7.0f,3.0f);
+        float max1 = Maximum.printMaximum(8.0f,7.0f,3.0f);
         Assert.assertEquals(8.0f, max1,0);
         System.out.println("Maximum No at First position:" +max1);
     }
 
     @Test
     public void maxFloatNumberAtSecondPosition_ReturnTheSameNumber() {
-        float max1 = Maximum.maxFloat(3.0f,7.0f,3.0f);
+        float max1 = Maximum.printMaximum(3.0f,7.0f,3.0f);
         Assert.assertEquals(7.0f, max1,0);
         System.out.println("Maximum No at Second position:" +max1);
     }
 
     @Test
     public void maxFloatNumberAtThirdPosition_ReturnTheSameNumber() {
-        float max1 = Maximum.maxFloat(4.0f,7.0f,8.0f);
+        float max1 = Maximum.printMaximum(4.0f,7.0f,8.0f);
         Assert.assertEquals(8.0f, max1,0);
         System.out.println("Maximum No at Third position:" +max1);
     }
 
     @Test
     public void maxStringAtFirstPosition_ReturnTheSameString() {
-        String max1 = Maximum.maxString("Peach","Banana","Apple");
+        String max1 = Maximum.printMaximum("Peach","Banana","Apple");
         Assert.assertEquals("Peach", max1);
         System.out.println("Maximum No at First position:" +max1);
     }
 
     @Test
     public void maxStringAtSecondPosition_ReturnTheSameString() {
-        String max1 = Maximum.maxString("Apple","Peach","Banana");
+        String max1 = Maximum.printMaximum("Apple","Peach","Banana");
         Assert.assertEquals("Peach", max1);
         System.out.println("Maximum No at Second position:" +max1);
     }
 
     @Test
     public void maxStringAtThirdPosition_ReturnTheSameString() {
-        String max1 = Maximum.maxString("Apple","Banana","Peach");
+        String max1 = Maximum.printMaximum("Apple","Banana","Peach");
         Assert.assertEquals("Peach", max1);
         System.out.println("Maximum No at Third position:" +max1);
     }
