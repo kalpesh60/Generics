df19ca01a7eba431122b3a2b047af25ac488ec9b
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-05-13 13:26:22 +0530

    Added code for printing the max

--- main/java/Maximum.java
+++ main/java/Maximum.java
@@ -7,51 +7,20 @@ public class Maximum <T extends Comparable<T>> {
         this.third = third;
     }
 
-//For multiple Parameters
+    //For multiple Parameters
     public static <T extends Comparable<T>> T multipleValues(T[] array) {
         T max = array[0];
-        for(int i=1; i<array.length; i++) {
-            if(array[i].compareTo(max) > 0)
+        for (int i = 1; i < array.length; i++) {
+            if (array[i].compareTo(max) > 0)
                 max = array[i];
         }
+        printMaximum(array, max);
         return max;
     }
 
-//For Three Parameters
-    public static <T extends Comparable<T>> T printMaximum(T first, T second, T third) {
-        T max = first;
-        if (second.compareTo(max) > 0)
-            max = second;
-        if (third.compareTo(max) > 0)
-            max = third;
-        return max;
+    public static <T> void printMaximum(T[] array, T max) {
+        for (int i = 1; i < array.length; i += array.length) {
+            System.out.println("Maximum is:" +max);
+        }
     }
-
 }
-//    public static int maxInteger(Integer firstNumber,Integer secondNumber,Integer thirdNumber) {
-//        int max = firstNumber;
-//        if(secondNumber.compareTo(max) > 0)
-//            max = secondNumber;
-//        if(thirdNumber.compareTo(max) > 0)
-//            max = thirdNumber;
-//        return max;
-//    }
-//
-//    public static float maxFloat(Float firstNumber, Float secondNumber, Float thirdNumber) {
-//        float max1 = firstNumber;
-//        if(secondNumber.compareTo(max1) > 0)
-//            max1 = secondNumber;
-//        if(thirdNumber.compareTo(max1) > 0)
-//            max1 = thirdNumber;
-//        return max1;
-//    }
-//
-//    public static String maxString(String firstString, String secondString, String thirdString) {
-//        String max2 = firstString;
-//        if(secondString.compareTo(max2) > 0)
-//            max2 = secondString;
-//        if(thirdString.compareTo(max2) > 0)
-//            max2 = thirdString;
-//        return max2;
-//    }
-//}
--- test/java/MaximumTest.java
+++ test/java/MaximumTest.java
@@ -4,95 +4,10 @@ import org.junit.Test;
 public class MaximumTest {
 
     @Test
-    public void maxNumberAtFirstPosition_ReturnTheSameNumber() {
-        int max = Maximum.printMaximum(7,3,4);
-        Assert.assertEquals(7,max);
-        System.out.println("Maximum No at First position:" +max);
-    }
-
-    @Test
-    public void maxNumberAtSecondPosition_ReturnTheSameNumber() {
-        int max = Maximum.printMaximum(4,6,3);
-        Assert.assertEquals(6,max);
-        System.out.println("Maximum No at Second position:" +max);
-    }
-
-    @Test
-    public void maxNumberAtThirdPosition_ReturnTheSameNumber() {
-        int max = Maximum.printMaximum(4,6,9);
-        Assert.assertEquals(9,max);
-        System.out.println("Maximum No at Third position:" +max);
-    }
-
-    @Test
-    public void maxFloatNumberAtFirstPosition_ReturnTheSameNumber() {
-        float max1 = Maximum.printMaximum(8.0f,7.0f,3.0f);
-        Assert.assertEquals(8.0f, max1,0);
-        System.out.println("Maximum No at First position:" +max1);
-    }
-
-    @Test
-    public void maxFloatNumberAtSecondPosition_ReturnTheSameNumber() {
-        float max1 = Maximum.printMaximum(3.0f,7.0f,3.0f);
-        Assert.assertEquals(7.0f, max1,0);
-        System.out.println("Maximum No at Second position:" +max1);
-    }
-
-    @Test
-    public void maxFloatNumberAtThirdPosition_ReturnTheSameNumber() {
-        float max1 = Maximum.printMaximum(4.0f,7.0f,8.0f);
-        Assert.assertEquals(8.0f, max1,0);
-        System.out.println("Maximum No at Third position:" +max1);
-    }
-
-    @Test
-    public void maxStringAtFirstPosition_ReturnTheSameString() {
-        String max1 = Maximum.printMaximum("Peach","Banana","Apple");
-        Assert.assertEquals("Peach", max1);
-        System.out.println("Maximum No at First position:" +max1);
-    }
-
-    @Test
-    public void maxStringAtSecondPosition_ReturnTheSameString() {
-        String max1 = Maximum.printMaximum("Apple","Peach","Banana");
-        Assert.assertEquals("Peach", max1);
-        System.out.println("Maximum No at Second position:" +max1);
-    }
-
-    @Test
-    public void maxStringAtThirdPosition_ReturnTheSameString() {
-        String max1 = Maximum.printMaximum("Apple","Banana","Peach");
-        Assert.assertEquals("Peach", max1);
-        System.out.println("Maximum No at Third position:" +max1);
-    }
-
-    @Test
-    public void maxValueOfInteger_ShouldReturnTrue() {
-        int result = Maximum.printMaximum(10,7,9);
-        Assert.assertEquals(result,10);
-        System.out.println(result);
-    }
-
-    @Test
-    public void maxValueOfFloat_ShouldReturnTrue() {
-        Double result = Maximum.printMaximum(10.0,11.0,9.0);
-        Assert.assertEquals(result,11.0,0);
-        System.out.println(result);
-    }
-
-    @Test
-    public void maxValueOfString_ShouldReturnTrue() {
-        String result = Maximum.printMaximum("Apple","Banana","Peach");
-        Assert.assertEquals(result,"Peach");
-        System.out.println(result);
-    }
-
-    @Test
     public void maxValueInteger_ShouldReturnTrue() {
         Integer array [] ={12,54,87,98,34};
         int result = Maximum.multipleValues(array);
         Assert.assertEquals(98,result);
-        System.out.println("maximum from multiple:" +result);
     }
 
     @Test
@@ -100,7 +15,6 @@ public class MaximumTest {
         Double array [] ={12.0,54.4,87.3,98.2,34.5};
         double result = Maximum.multipleValues(array);
         Assert.assertEquals(98.2,result,0);
-        System.out.println("maximum from multiple:" +result);
     }
 
     @Test
@@ -108,6 +22,5 @@ public class MaximumTest {
         String array [] ={"Banana","Peach","Apple","Mango","Orange"};
         String result = Maximum.multipleValues(array);
         Assert.assertEquals("Peach",result);
-        System.out.println("maximum from multiple:" +result);
     }
 }
